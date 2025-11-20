c2e040dd26ae9ed50b5233cedb466d73cbff8bbf
unknown <damonlam315@gmail.com>
2025-11-19 22:47:41 -0500

    booking updates

--- EECS-3311-Project/src/main/java/org/openjfx/EECS_3311_Project/model/Booking.java
+++ EECS-3311-Project/src/main/java/org/openjfx/EECS_3311_Project/model/Booking.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.UUID;
 
-public class Booking {
+public class Booking implements ICSVDataObject{
 	private String id;
     private String roomId;
     private String name;
@@ -16,6 +16,43 @@ public class Booking {
     private LocalDateTime checkInTime;
     private Status status;
     
+    
+    public Booking(String csvRow) {
+        // id, roomId, name, checkedin, host, atds, starttime, endtime, checkintime, status
+        String[] tokens = csvRow.split(",");
+        this.id = tokens[0];
+        this.roomId = tokens[1];
+        this.name = tokens[2];
+        this.isCheckedIn = tokens[3].isEmpty() ? null : Boolean.parseBoolean(tokens[3]); 
+
+        String hostId = tokens[4];
+        this.host = new User();
+        this.host.setId(hostId);
+        
+        //attendees can be empty arraylist
+        this.attendees = new ArrayList<>();
+        String attendeeList = tokens[5];
+        if(!"[]".equals(attendeeList)) { //if list isnt empty
+        	String attendeeLine = attendeeList.substring(1, attendeeList.length() - 1); //remove the []
+        	String[] attendeeID = attendeeLine.split(";"); //remove ;
+        	
+        	for(String userId : attendeeID) {
+        		String cleanedIds = userId.trim(); //remove whitespace
+        		User user = new User();
+        		user.setId(hostId);
+        		this.attendees.add(user);
+        	}
+        
+        }
+        
+        this.startTime = LocalDateTime.parse(tokens[6]);
+        this.endTime = LocalDateTime.parse(tokens[7]);
+        this.checkInTime = LocalDateTime.parse(tokens[8]);
+        this.status = Status.valueOf(tokens[9]);
+        
+        
+        
+    }
 	public Booking(String id, String roomId, String name, Boolean isCheckedIn, User host, ArrayList<User> attendees, LocalDateTime startTime, LocalDateTime endTime, LocalDateTime checkInTime, Status status) {
 		this.id = id;
 		this.roomId = roomId;
@@ -58,8 +95,12 @@ public class Booking {
 		this.isCheckedIn = false;
 	}
 	
-	public String getHostUserId() {
-		return host.getId();
+	public User getHost() {
+		return host;
+	}
+	
+	public void setHost(User host) {
+		this.host = host;
 	}
 	
 	public ArrayList<String> getInvitedUserIds() {
@@ -111,6 +152,27 @@ public class Booking {
 
 	public void setAttendees(ArrayList<User> attendees) {
 	    this.attendees = attendees;
+	}
+
+	@Override
+	public String toCSVRow() {
+		// TODO Auto-generated method stub
+		String hostId = host.getId();
+		String atds = "[]";
+		ArrayList<String> invitedppl = getInvitedUserIds();
+		if(!invitedppl.isEmpty()) {
+			atds = "[" + String.join(";", invitedppl)+"]";
+			
+		}
+		String checkedIn = (isCheckedIn != null) ? isCheckedIn.toString():"";
+		String start = startTime.toString();
+		String end = endTime.toString();
+		String checkedTime = (checkInTime != null)? checkInTime.toString(): "";
+		String statusString = status.name();
+		
+		
+		
+		return String.join(",", id, roomId, name, checkedIn, hostId, atds, start, end, checkedTime, statusString);
 	}
 	
 	
